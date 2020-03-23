d32a1996ab3d56231e1559be4f40ebdfe072c08f
Helder Carmen <heldercarmen@gmail.com>
2020-03-23 08:56:44 +0000

    fix spacing

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,13 +1,23 @@
-function FizzBuzz(number){
-    if(number%15 === 0){
+
+
+function FizzBuzz(i){
+
+//for(i=1; i<=number; i++){
+    if(i%3===0 && i%5 === 0){
         return 'FizzBuzz';
-    }else if(number%3 === 0){
+    }
+    else if(i%3 === 0){
         return 'Fizz';
-    }else if(number%5 === 0){
+    }
+    else if(i%5 === 0){
         return 'Buzz';
-    }else{
+    }
+    else{
         return number;
     }
   
 
+//}
+
 }
+
